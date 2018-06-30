6253590323edd7a8a7a47c8f9749316eae763ce9
Torsten Schmickler <torsten.schmickler@gmail.com>
2018-06-30 15:17:32 +0530

    Initial commit

--- habit_tracker/DashboardViewController.swift
+++ habit_tracker/DashboardViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    let userDefaults = UserDefaults.standard
+    let sevenDaysInSeconds: TimeInterval = 604800
+    var habitData: [String:[Date]] = [:]
+    
+    @IBOutlet var habitButtons: [UIButton]!
+    @IBOutlet var habitCounterLabels: [UILabel]!
+    
+    
+    // Mehtods
+    @IBAction func touchHabit(_ sender: UIButton) {
+        let habitIndex = habitButtons.index(of: sender)!
+        let habitKey = String(habitIndex)
+        if habitData[habitKey] != nil {
+            habitData[habitKey]!.append(Date())
+        } else {
+            habitData[habitKey] = [Date()]
+        }
+        userDefaults.set(habitData, forKey: "habitData")
+        print(userDefaults.dictionary(forKey: "habitData")!)
+        habitCounterLabels[habitIndex].text = String(countTimesLastSevenDays(habitData[habitKey]!))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadCounters()
+    }
+    
+    func loadCounters() {
+        if let storedData = (userDefaults.dictionary(forKey: "habitData") as! Dictionary<String, [Date]>?) {
+            var index = 0
+            var count = 0
+            habitData = storedData
+            habitCounterLabels.forEach { habitLabel in
+                if let dates = storedData[String(index)] {
+                    count = countTimesLastSevenDays(dates)
+                    habitLabel.text = String(count)
+                }
+                index += 1
+            }
+        }
+    }
+    
+    func countTimesLastSevenDays(_ arrayOfDates: Array<Date>) -> Int{
+        let sevenDaysAgo = Date().addingTimeInterval(-sevenDaysInSeconds)
+        var times = 0
+        arrayOfDates.forEach { date in
+            if date > sevenDaysAgo {
+                times += 1
+            }
+        }
+        return times
     }
 
     override func didReceiveMemoryWarning() {
